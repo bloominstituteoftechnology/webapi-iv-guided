81a1224ae690b03f41633fd3ff9c2b9ba613f210
brettmadrid <brettmadrid9@gmail.com>
2019-03-06 19:53:58 -0800

    added dotnv

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('./api/server.js');
 
 // we'll read the port from the server environment if it is there
 // heroku will have the PORT environment variable set
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
 server.listen(port, () => {
