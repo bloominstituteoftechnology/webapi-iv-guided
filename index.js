88ebbd6fee970d44089f70d3a8ea7d304eaa3f98
fbt328 <katherine.rust.andrews@gmail.com>
2019-03-07 19:26:37 -0800

    added .env file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const server = require('./api/server.js');
+PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on PORT ${PORT} ***\n`);
 });
