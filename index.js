5c3bfd3d584b57ac706685e02445ae1919f57046
Pav0l <pavol.karas@gmail.com>
2019-03-21 10:43:10 +0100

    Setup PORT and start script for Heroku

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// you need to specify proces.env.PORT for Heroku to work,
+// but oin development environment, it won't work (it's undefined) ,so you
+// need a condition to add port number
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
