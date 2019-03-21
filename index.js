fc608624eb141a7f0546f68773946e27e69cd0dc
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-21 12:14:15 -0500

    Added stuff.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+  console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
