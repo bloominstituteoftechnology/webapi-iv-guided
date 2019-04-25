00fec6bbec940af61b03446337c9f059e673d436
Alexnadria Supriana <alexa9410@gmail.com>
2019-04-25 19:12:54 -0400

    caps

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const envReader = require("dotenv");
 envReader.config;
 const server = require("./api/server.js");
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 server.listen(4000, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
 });
