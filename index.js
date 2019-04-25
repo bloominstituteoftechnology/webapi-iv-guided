7427619642a41f24268b350f25b6905e75fb63f5
Alexnadria Supriana <alexa9410@gmail.com>
2019-04-25 19:41:26 -0400

    port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ envReader.config;
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 4000;
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log("\n*** Server Running on http://localhost:4000 ***\n");
 });
