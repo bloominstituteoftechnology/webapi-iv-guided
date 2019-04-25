76ed345343a50f970666a1ba2556c397dc4c80e1
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-25 12:46:40 -0400

    Made port dynamic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
