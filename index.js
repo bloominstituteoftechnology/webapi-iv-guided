f1f591ff9eb3bf6adece3e3ce8cbbe698c36ff4a
J-E-M <javonmccoy22@gmail.com>
2019-05-30 12:02:52 -0500

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./api/server.js');
 
 const port = process.env.port;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
 // hi
