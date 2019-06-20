c3876cdf0bb5e2ad461bfab3eb5c3f00c84d8da4
kingtamaaj <kingtamaaj@gmail.com>
2019-06-20 13:30:56 -0400

    first

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+// it is recommended to load configuration for .env as early as possible
+require('dotenv').config() //first thing to run
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// we'll rad the port from there server environment if it is there 
+// heroku will have the port environment variable set
+
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 500 as default
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+//help
