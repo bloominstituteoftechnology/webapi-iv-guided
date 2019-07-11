01c0d6e483b7957682975b2fbd2d1d7af44a35a9
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-07-11 04:42:29 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -8,16 +8,29 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+// server.get('/', async (req, res) => {
+//   try {
+//     const shouts = await db('shouts');
+//     res.status(200).json(shouts);
+//     throw new Error('for testing only');
+//   } catch (error) {
+//     console.error('\nERROR', error);
+//     res.status(500).json({ error: 'Cannot retrieve the shouts' });
+//   }
+// });
+
 server.get('/', async (req, res) => {
-  try {
-    const shouts = await db('shouts');
-    res.status(200).json(shouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shouts' });
-  }
+  db('shouts')
+    .then(shouts => {
+      res.status(200).json(shouts);
+
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Cannot retrieve the shouts' });
+    })
 });
 
+// ???????
 server.post('/', async (req, res) => {
   try {
     const [id] = await db('shouts').insert(req.body);
