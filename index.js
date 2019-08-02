fe78359e70eb6d9635d46c8eead4f464dbee3683
Ben Q <Quackenbush117@gmail.com>
2019-08-01 20:30:34 -0600

    first

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server.js');
+require('dotenv').config();
+const port = process.env.PORT ? process.env.PORT : 4000
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
