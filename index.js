12d98c52476e59f767e79de6f803501a283eeff9
quan005 <ralphealfreeman@gmail.com>
2019-08-14 17:44:39 -0700

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on Port ${port} ***\n`);
 });
 
