4ee9691d13e6106ac9bfad15954d8dea52775e38
Zac Smith <zrs3141592@gmail.com>
2019-08-17 08:45:22 -0600

    initial commit, dynamic port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
+const port = 4000;
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
