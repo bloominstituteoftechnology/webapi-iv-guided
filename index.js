cc0c7675804e0cba14609c2d815292bed2057e55
Adrian Coleman <actyler@Adrians-MacBook-Pro.local>
2019-11-06 23:03:45 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const server = require('./api/server.js');
+require("dotenv").config();
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 5000;
+//process.env.PORT makes port dynamic.  To start server, in terminal type export PORT=4000 to set desired port, then start server
+
+//DESTRUCTURING PORT
+//const {PORT} = process.env
+//server.listen(PORT, () => {
+// console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)})
 
 server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
