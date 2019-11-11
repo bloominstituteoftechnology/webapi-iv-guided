1b6db93afedbf7100f66822a6217dd383f06c740
Toosdai Otte <toosdai52Otte@gmail.com>
2019-11-11 00:59:18 -0600

    added dotenv, added start script

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
