a7f527ae07cbef686e0224014c9fd1ad1ab97f05
Ale <alearanibar@gmail.com>
2019-12-05 10:57:04 -0800

    env3

--- api/server.js
+++ api/server.js
@@ -9,9 +9,10 @@ server.use(helmet());
 server.use(express.json());
 
 server.get('/',  (req, res) => {
+  const message = process.env.MSG || "Hello world"
   Shoutouts.find()
   .then(shoutouts => {
-    res.status(200).json(shoutouts);
+    res.status(200).json({message, shoutouts});
   })
   .catch (error => {
     console.error('\nERROR', error);
