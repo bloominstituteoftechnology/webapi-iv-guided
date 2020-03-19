5a8246ea53099ed9b475b1069298408c8b0d9ffa
Catherine Kim <catherinesjkim@gmail.com>
2020-03-19 09:27:42 -0700

    adds dynamic port and start script

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 
 const Shoutouts = require('../data/shoutouts-model.js');
+// const apiRouter = require('./api-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+// server.use('/api', apiRouter);
 
 server.get('/', (req, res) => {
   Shoutouts.find()
