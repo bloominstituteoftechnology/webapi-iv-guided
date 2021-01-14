9cd19401f4d6f0d9c0f9c942c211867e1ce307f2
crystal-csete <crystal-csete@lambdastudents.com>
2021-01-14 08:41:15 -0700

    added

--- api/server.js
+++ api/server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const Shoutouts = require('../data/shoutouts-model.js');
+const db = require('../data/db.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  Shoutouts.find()
-  .then(shoutouts => {
-    res.status(200).json(shoutouts);
-  })
-  .catch (error => {
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  });
+  }
 });
 
-server.post('/', (req, res) => {
-  Shoutouts.add(req.body)
-  .then(shoutout => {
-    res.status(201).json(shoutout);
-  })
-  .catch (error => {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  });
+server.post('/', async (req, res) => {
+  // shoutouts.add(req.body)
+  // .then(shoutouts => {
+  //   res.status(201).json(shoutouts);
+  // })
+  // .catch (error => {
+  //   console.error('\nERROR', error);
+  //   res.status(500).json({ error: 'Cannot add the shoutout' });
+  // });
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
 });
